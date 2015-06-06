38c420dcba3d0c06d43993b4fd0736c929220889
Thiago Prado <tprado@thoughtworks.com>
2015-06-06 16:54:06 -0300

    Replaces 'for' loop by 'forEach' call.

--- src/main/java/j8spec/junit/J8SpecRunner.java
+++ src/main/java/j8spec/junit/J8SpecRunner.java
@@ -65,9 +65,7 @@ public final class J8SpecRunner extends ParentRunner<ItBlock> {
     protected void runChild(ItBlock itBlock, RunNotifier notifier) {
         notifier.fireTestStarted(describeChild(itBlock));
         try {
-            for (Runnable beforeEachBlock : itBlock.beforeEachBlocks()) {
-                beforeEachBlock.run();
-            }
+            itBlock.beforeEachBlocks().forEach(Runnable::run);
             itBlock.getBody().run();
         } catch (Throwable t) {
             notifier.fireTestFailure(new Failure(describeChild(itBlock), t));
