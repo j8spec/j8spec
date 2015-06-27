354d920a8c8344005ba24206930e99c2b35ebbdb
Thiago Prado <tprado@thoughtworks.com>
2015-06-27 17:19:08 -0300

    Adds test to spec with no before each block.

--- src/test/java/j8spec/J8SpecBeforeEachTest.java
+++ src/test/java/j8spec/J8SpecBeforeEachTest.java
@@ -5,41 +5,48 @@ import org.junit.Test;
 
 import static j8spec.J8Spec.*;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class J8SpecBeforeEachTest {
 
-    private static final Runnable BEFORE_EACH_BLOCK = () -> {};
-    private static final Runnable BEFORE_EACH_A_BLOCK = () -> {};
-    private static final Runnable BEFORE_EACH_AA_BLOCK = () -> {};
-    private static final Runnable BEFORE_EACH_B_BLOCK = () -> {};
+    private static final Runnable NOOP = () -> {};
+    
+    private static final Runnable BEFORE_EACH_BLOCK = NOOP;
+    private static final Runnable BEFORE_EACH_A_BLOCK = NOOP;
+    private static final Runnable BEFORE_EACH_AA_BLOCK = NOOP;
+    private static final Runnable BEFORE_EACH_B_BLOCK = NOOP;
 
     static class BeforeEachBlockOverwrittenSpec {{
-        beforeEach(() -> {});
-        beforeEach(() -> {});
+        beforeEach(NOOP);
+        beforeEach(NOOP);
+    }}
+
+    static class NoBeforeEachSpec {{
+        it("block 1", NOOP);
     }}
 
     static class SampleSpec {{
         beforeEach(BEFORE_EACH_BLOCK);
 
-        it("block 1", () -> {});
+        it("block 1", NOOP);
 
         describe("describe A", () -> {
             beforeEach(BEFORE_EACH_A_BLOCK);
 
-            it("block A.1", () -> {});
+            it("block A.1", NOOP);
 
             describe("describe A.A", () -> {
                 beforeEach(BEFORE_EACH_AA_BLOCK);
 
-                it("block A.A.1", () -> {});
+                it("block A.A.1", NOOP);
             });
         });
 
         describe("describe B", () -> {
             beforeEach(BEFORE_EACH_B_BLOCK);
 
-            it("block B.1", () -> {});
+            it("block B.1", NOOP);
         });
     }}
 
@@ -58,12 +65,19 @@ public class J8SpecBeforeEachTest {
 
     @Test(expected = IllegalContextException.class)
     public void does_not_allow_before_each_method_direct_invocation() {
-        beforeEach(() -> {});
+        beforeEach(NOOP);
     }
 
     @Test(expected = BlockAlreadyDefinedException.class)
     public void does_not_allow_before_each_block_to_be_replaced() {
         executionPlanFor(BeforeEachBlockOverwrittenSpec.class);
+    }
+
+    @Test
+    public void builds_an_execution_plan_when_there_is_no_before_each_block() {
+        ExecutionPlan plan = executionPlanFor(NoBeforeEachSpec.class);
+
+        assertThat(plan.beforeEachBlock(), is(nullValue()));
     }
 
     @Test
