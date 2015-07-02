11e66f549e15893db860dec4919eba6566fc8280
Thiago Prado <tprado@thoughtworks.com>
2015-07-01 22:55:22 -0300

    Introduces "j8spec.ci.mode" system property in order to prevent xdescribe/xit/fdescribe/fit in the CI.

---
+++ src/main/java/j8spec/CIModeEnabledException.java
@@ -0,0 +1,10 @@
+package j8spec;
+
+/**
+ * @since 1.1.0
+ */
+public class CIModeEnabledException extends J8SpecException {
+    CIModeEnabledException(String message) {
+        super(message);
+    }
+}
