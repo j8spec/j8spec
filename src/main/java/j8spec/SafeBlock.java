5f7e5ef3745d2eb97f1d9720ea8bb2e93edce714
Thiago Prado <thiago.gozzi.prado@gmail.com>
2016-02-26 15:57:55 -0300

    Update SafeBlock/UnsafeBlock Javadocs

--- src/main/java/j8spec/SafeBlock.java
+++ src/main/java/j8spec/SafeBlock.java
@@ -1,6 +1,8 @@
 package j8spec;
 
 /**
+ * Block of code that is safe to execute, i.e. it does not throw checked exceptions.
+ *
  * @see j8spec.UnsafeBlock
  * @since 3.0.0
  */
@@ -9,5 +11,8 @@ public interface SafeBlock {
 
     SafeBlock NOOP = () -> {};
 
+    /**
+     * Execute the block of code.
+     */
     void execute();
 }
--- src/main/java/j8spec/UnsafeBlock.java
+++ src/main/java/j8spec/UnsafeBlock.java
@@ -1,6 +1,8 @@
 package j8spec;
 
 /**
+ * Block of code that is unsafe to execute, i.e. it could throw checked exceptions.
+ *
  * @see j8spec.SafeBlock
  * @since 3.0.0
  */
@@ -9,5 +11,10 @@ public interface UnsafeBlock {
 
     UnsafeBlock NOOP = () -> {};
 
+    /**
+     * Try to execute the block of code.
+     *
+     * @throws Throwable if unable to execute the block of code
+     */
     void tryToExecute() throws Throwable;
 }
